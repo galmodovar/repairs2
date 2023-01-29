3665aa1af7d2de9703d86c118aa581411d65eef5
galmodovar <galmodovar1@gmail.com>
2023-01-29 12:28:47 -0600

    working through chapters

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,70 @@
-export const TicketList = () => {
-    return <h2>List of Tickets</h2>
-}
+import { useEffect, useState } from "react"
+import "./TicketList.css"
 
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFilteredTickets] = useState([])
+    const [emergency, setEmergency] = useState(false)
+    const localHoneyUser = localStorage.getItem('honey_user')
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    useEffect(
+        ()=> {
+            if (emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFilteredTickets(emergencyTickets)
+            }
+        },
+        [emergency]
+    )
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/serviceTickets")
+            .then(res => res.json())
+            .then((data) => {
+                setTickets(data)
+            })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+    useEffect(
+        () => {
+            if (honeyUserObject.staff) {
+                setFilteredTickets(tickets)
+            }
+            else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id )
+                setFilteredTickets(myTickets)
+            }
+        },[tickets]
+    )
+    return <>
+    {
+        honeyUserObject.staff ? 
+        <button
+            onClick={
+                () => {
+                    setEmergency(true)
+                }
+            }>Emergency Only
+        </button> : ""
+    }
+    <h2>List of Tickets</h2>
+
+    <article className='tickets'>
+        {
+            filteredTickets.map(
+                (ticket) => {
+                    return <section className='ticket' key={`ticket--${ticket.id}`}>
+                        <header> {ticket.description} </header>
+                        <footer> Emergency: {ticket.emergency ? 'Yes' : 'No'} </footer>
+                    </section>
+                }
+            )
+        }
+    </article>
+    </> 
+
+}
