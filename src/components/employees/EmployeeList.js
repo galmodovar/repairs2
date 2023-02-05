5ee5e2327f4aa2fdddd1e311d505ba79f7cafbaf
galmodovar <galmodovar1@gmail.com>
2023-02-05 16:39:07 -0600

    format

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -1,30 +1,27 @@
-import { useState, useEffect } from "react"
-import './Employees.css'
+import { useState, useEffect } from "react";
+import "./Employees.css";
 
 export const EmployeeList = () => {
-    const [employees, setEmployees] = useState([])
+  const [employees, setEmployees] = useState([]);
 
-    useEffect(
-        () => {
-             fetch('http://localhost:8088/users?isStaff=true')
-                .then(res => res.json())
-                .then((emp) => {
-                setEmployees(emp)
-            })
-    }, []
-    )
+  useEffect(() => {
+    fetch("http://localhost:8088/users?isStaff=true")
+      .then((res) => res.json())
+      .then((emp) => {
+        setEmployees(emp);
+      });
+  }, []);
 
-    return <article className='employees'>
-        {
-            
-            employees.map(employee => {
-                return <section className='employee' key={`employee--${employee.id}`}>
-                    <div>Name: {employee.fullName}</div>
-                    <div>Email: {employee.email}</div>
-                </section>
-            })
-        }
-
+  return (
+    <article className="employees">
+      {employees.map((employee) => {
+        return (
+          <section className="employee" key={`employee--${employee.id}`}>
+            <div>Name: {employee.fullName}</div>
+            <div>Email: {employee.email}</div>
+          </section>
+        );
+      })}
     </article>
-    
-}
+  );
+};
--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,109 +1,102 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import "./TicketList.css"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./TicketList.css";
 
 export const TicketList = ({ searchState }) => {
-    const [tickets, setTickets] = useState([])
-    const [filteredTickets, setFilteredTickets] = useState([])
-    const [emergency, setEmergency] = useState(false)
-    const [openOnly, updateOpenOnly] = useState(false)
-    const navigate = useNavigate()
+  const [tickets, setTickets] = useState([]);
+  const [filteredTickets, setFilteredTickets] = useState([]);
+  const [emergency, setEmergency] = useState(false);
+  const [openOnly, updateOpenOnly] = useState(false);
+  const navigate = useNavigate();
 
+  const localHoneyUser = localStorage.getItem("honey_user");
+  const honeyUserObject = JSON.parse(localHoneyUser);
 
-    const localHoneyUser = localStorage.getItem('honey_user')
-    const honeyUserObject = JSON.parse(localHoneyUser)
+  useEffect(() => {
+    const searchTickets = tickets.filter((ticket) =>
+      ticket.description.startsWith(searchState)
+    );
+    setFilteredTickets(searchTickets);
+  }, [searchState]);
 
-    useEffect(
-        () => {
-            const searchTickets = tickets.filter(ticket => ticket.description.startsWith(searchState))
-            setFilteredTickets(searchTickets)
+  useEffect(() => {
+    if (emergency) {
+      const emergencyTickets = tickets.filter(
+        (ticket) => ticket.emergency === true
+      );
+      setFilteredTickets(emergencyTickets);
+    } else {
+      setFilteredTickets(tickets);
+    }
+  }, [emergency]);
 
-        },
-        [searchState]
-    )
+  useEffect(
+    () => {
+      fetch("http://localhost:8088/serviceTickets")
+        .then((res) => res.json())
+        .then((data) => {
+          setTickets(data);
+        });
+    },
+    [] // When this array is empty, you are observing initial component state
+  );
+//FILTERED
+  useEffect(() => {
+    if (honeyUserObject.staff) {
+      setFilteredTickets(tickets);
+    } else {
+      const myTickets = tickets.filter(
+        (ticket) => ticket.userId === honeyUserObject.id
+      );
+      setFilteredTickets(myTickets);
+    }
+  }, [tickets]);
+//OPEN
+  useEffect(() => {
+    if (openOnly) {
+      const openTicketArr = tickets.filter((ticket) => {
+        return (
+          ticket.userId === honeyUserObject.id && ticket.dateCompleted === ""
+        );
+      });
+      setFilteredTickets(openTicketArr);
+    } else {
+      const myTickets = tickets.filter(
+        (ticket) => ticket.userId === honeyUserObject.id
+      );
+      setFilteredTickets(myTickets);
+    }
+  }, [openOnly]);
+  
+  return (
+    <>
+      {honeyUserObject.staff ? (
+        <>
+          <button onClick={() => { setEmergency(true); }}> Emergency Only </button>
+          <button onClick={() => { setEmergency(false); }}> Show All </button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => navigate("/ticket/create")}>
+            {" "}
+            Create Tickets{" "}
+          </button>
+          <button onClick={() => updateOpenOnly(true)}> Open Tickets </button>
+          <button onClick={() => updateOpenOnly(false)}> All Tickets </button>
+        </>
+      )}
+      <h2>List of Tickets</h2>
 
-    useEffect(
-        () => {
-            if (emergency) {
-                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
-                setFilteredTickets(emergencyTickets)
-            }
-            else {
-                setFilteredTickets(tickets)
-            }
-        },
-        [emergency]
-    )
-
-    useEffect(
-        () => {
-            fetch("http://localhost:8088/serviceTickets")
-                .then(res => res.json())
-                .then((data) => {
-                    setTickets(data)
-                })
-        },
-        [] // When this array is empty, you are observing initial component state
-    )
-
-    useEffect(
-        () => {
-            if (honeyUserObject.staff) {
-                setFilteredTickets(tickets)
-            }
-            else {
-                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
-                setFilteredTickets(myTickets)
-            }
-        }, [tickets]
-    )
-
-    useEffect(
-        () => {
-            if (openOnly) {
-
-                const openTicketArr = tickets.filter(ticket => {
-                    return ticket.userId === honeyUserObject.id && ticket.dateCompleted === ""
-                })
-                setFilteredTickets(openTicketArr)
-            }
-            else {
-                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
-                setFilteredTickets(myTickets)
-            }
-
-        },
-        [openOnly]
-    )
-    return <>
-        {
-            honeyUserObject.staff ?
-                <>
-                    <button onClick={() => { setEmergency(true) }}> Emergency Only </button>
-                    <button onClick={() => { setEmergency(false) }}> Show All </button>
-                </>
-                :
-                <>
-                    <button onClick={() => navigate("/ticket/create")}> Create Tickets </button>
-                    <button onClick={() => updateOpenOnly(true)}> Open Tickets </button>
-                    <button onClick={() => updateOpenOnly(false)}> All Tickets </button>
-
-                </>
-        }
-        <h2>List of Tickets</h2>
-
-        <article className='tickets'>
-            {
-                filteredTickets.map(
-                    (ticket) => {
-                        return <section className='ticket' key={`ticket--${ticket.id}`}>
-                            <header> {ticket.description} </header>
-                            <footer> Emergency: {ticket.emergency ? 'Yes' : 'No'} </footer>
-                        </section>
-                    }
-                )
-            }
-        </article>
+      <article className="tickets">
+        {filteredTickets.map((ticket) => {
+          return (
+            <section className="ticket" key={`ticket--${ticket.id}`}>
+              <header> {ticket.description} </header>
+              <footer> Emergency: {ticket.emergency ? "Yes" : "No"} </footer>
+            </section>
+          );
+        })}
+      </article>
     </>
-
-}
+  );
+};
