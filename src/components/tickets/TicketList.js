f2a689c1f196a99bc50fe2a6ad5d70fdf30c5981
galmodovar <galmodovar1@gmail.com>
2023-02-04 19:26:56 -0600

    search tickets

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./TicketList.css"
 
-export const TicketList = () => {
+export const TicketList = ({ searchState }) => {
     const [tickets, setTickets] = useState([])
     const [filteredTickets, setFilteredTickets] = useState([])
     const [emergency, setEmergency] = useState(false)
@@ -12,6 +12,15 @@ export const TicketList = () => {
 
     const localHoneyUser = localStorage.getItem('honey_user')
     const honeyUserObject = JSON.parse(localHoneyUser)
+
+    useEffect(
+        () => {
+            const searchTickets = tickets.filter(ticket => ticket.description.startsWith(searchState))
+            setFilteredTickets(searchTickets)
+
+        },
+        [searchState]
+    )
 
     useEffect(
         () => {
