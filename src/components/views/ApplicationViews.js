7f6e8189f5883235a1f115d954e8da5b209414a1
galmodovar <galmodovar1@gmail.com>
2023-02-04 20:23:04 -0600

    employee/customer views

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,27 +1,21 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { TicketContainer } from "../tickets/TicketContainer"
-import { TicketForm } from "../tickets/TicketForm"
+import { CustomerViews } from "./CustomerViews"
+import { EmployeeViews } from "./EmployeeViews"
 
 export const ApplicationViews = () => {
-	return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <h1>Honey Rae Repair Shop</h1>
-                    <div>Your one-stop-shop to get all your electronics fixed</div>
 
-                    <Outlet />
-                </>
-            }>
+    const localHoneyUser = localStorage.getItem('honey_user')
+    const honeyUserObject = JSON.parse(localHoneyUser)
 
-                <Route path="tickets" element={
-                    <>
-                        <TicketContainer />
-                    </>
-                } />
-                <Route path="ticket/create" element={ <TicketForm /> } />
-            </Route>
-        </Routes>
-    )
+    if (honeyUserObject.staff) {
+        return <EmployeeViews />
+
+    }
+    else {
+
+        return <CustomerViews />
+
+    }
+
+	
 }
 
